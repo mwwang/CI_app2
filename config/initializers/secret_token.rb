24a2a16f18ff82c93c80c831704af6e9030c811d
Mark Wang <mwwang@gmail.com>
2012-10-01 20:08:44 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cardinform::Application.config.secret_token = '17712fe51a3f40c7e734526297ea049b105f36b05823f6c15bbd5ee50af574155478d45e9196d749cca4de8dd45533e6526a4bf2629a8a21bd4badf8c88d0024'
